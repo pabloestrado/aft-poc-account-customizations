a09b7783f3865d28dc8860c3bc54b88417581362
Pavlo Romaniuk <pavlo.romaniuk@automat-it.com>
2024-10-07 15:16:13 +0300

    Test

--- eks-public/terraform/rds.tf
+++ eks-public/terraform/rds.tf
@@ -1,5 +1,5 @@
 module "sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/mysql"
+  source = "terraform-aws-modules/security-group/aws//modules/postgresql"
 
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
@@ -8,33 +8,27 @@ module "sg" {
   ingress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
 }
 
-# module "db" {
-#   source = "terraform-aws-modules/rds/aws"
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
 
-#   identifier = "${local.basename}-db"
+  identifier = "${local.basename}-db"
 
-#   engine            = "mysql"
-#   engine_version    = "8.0"
-#   instance_class    = "db.t4g.micro"
-#   allocated_storage = 5
+  engine            = "postgres"
+  engine_version    = "16.3"
+  instance_class    = "db.t4g.micro"
+  allocated_storage = 5
 
-#   db_name  = "app"
-#   username = "admin"
-#   port     = "3306"
+  db_name  = "app"
+  username = "admin"
 
-#   iam_database_authentication_enabled = true
+  vpc_security_group_ids = [module.sg.security_group_id]
 
-#   vpc_security_group_ids = [module.sg.security_group_id]
+  multi_az = false
 
-#   multi_az = false
+  # DB subnet group
+  create_db_subnet_group = true
+  subnet_ids             = module.vpc.private_subnets
 
-#   # DB subnet group
-#   create_db_subnet_group = true
-#   subnet_ids             = module.vpc.private_subnets
-
-#   # DB parameter group
-#   family = "mysql8.0"
-
-#   # DB option group
-#   major_engine_version = "8.0"
-# }
+  # DB parameter group
+  family = "postgres16.3"
+}
