445fb711197820cca2b8651773306f21ccdb4b64
Pavlo Romaniuk <pavlo.romaniuk@automat-it.com>
2024-10-21 14:27:30 +0300

    Add network orchectration hub

---
+++ mgmt/terraform/network-orchestration-hub.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudformation_stack" "hub" {
+  name         = "network-orchestration-hub"
+  template_url = "https://solutions-reference.s3.amazonaws.com/network-orchestration-for-aws-transit-gateway/latest/network-orchestration-hub.template"
+
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+
+  parameters = {
+    PrincipalType                           = "AWS Organization ARN"
+    Principals                              = "arn:aws:organizations::444629336067:organization/o-mmrpss74fy"
+    ConsoleLoginInformationEmail            = "pavlo.romaniuk@automat-it.com"
+    ApprovalNotificationEmail               = "pavlo.romaniuk@automat-it.com"
+    DeployWebUi                             = "Yes"
+    CognitoDomainPrefixParameter            = "aft-sandbox"
+    CognitoSAMLProviderNameParameter        = ""
+    CognitoSAMLProviderMetadataUrlParameter = ""
+  }
+
+}
