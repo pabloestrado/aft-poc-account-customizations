056b8c9c787c2ec2c492fc9fe572832f4ed2373f
Pavlo Romaniuk <pavlo.romaniuk@automat-it.com>
2024-10-22 12:49:01 +0300

    Network orchestration hub fix: add role arn

--- mgmt/terraform/network-orchestration-hub.tf
+++ mgmt/terraform/network-orchestration-hub.tf
@@ -5,6 +5,7 @@ resource "aws_cloudformation_stack" "hub" {
   capabilities = ["CAPABILITY_NAMED_IAM"]
 
   parameters = {
+    OrganizationManagementAccountRoleArn    = "arn:aws:iam::444629336067:role/network-orchestration-org-OrganizationInformationRo-p0ac7QdQDf0Z"
     PrincipalType                           = "AWS Organization ARN"
     Principals                              = "arn:aws:organizations::444629336067:organization/o-mmrpss74fy"
     ConsoleLoginInformationEmail            = "pavlo.romaniuk@automat-it.com"
