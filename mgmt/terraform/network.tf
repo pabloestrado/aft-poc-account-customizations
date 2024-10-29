c41c7ea85080677388dc3dd77c861de11b4bec28
Pavlo Romaniuk <pavlo.romaniuk@automat-it.com>
2024-10-29 13:37:37 +0200

    Deploy ipam #2

--- mgmt/terraform/network.tf
+++ mgmt/terraform/network.tf
@@ -1,33 +1,39 @@
-# ### VPC: MGMT
+### VPC: MGMT
 
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = local.basename
+  name = local.basename
 
-#   use_ipam_pool     = true
-#   ipv4_ipam_pool_id = aws_vpc_ipam_pool.prod.id
-#   cidr              = aws_vpc_ipam_preview_next_cidr.preview_cidr.cidr
+  use_ipam_pool     = true
+  ipv4_ipam_pool_id = aws_vpc_ipam_pool.mgmt.id
+  cidr              = data.aws_vpc_ipam_preview_next_cidr.preview_cidr.cidr
 
-#   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-#   private_subnets = local.vpc_private_subnets
-#   public_subnets  = local.vpc_public_subnets
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = local.vpc_private_subnets
+  public_subnets  = local.vpc_public_subnets
 
-#   enable_nat_gateway = false
-#   single_nat_gateway = true
-# }
+  enable_nat_gateway = false
+  single_nat_gateway = true
 
-# resource "aws_vpc_ipam_preview_next_cidr" "preview_cidr" {
-#   ipam_pool_id = aws_vpc_ipam_pool.prod.id
-# }
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.mgmt
+  ]
+}
 
-# locals {
-#   ipam_cidr = aws_vpc_ipam_preview_next_cidr.preview_cidr.cidr
+data "aws_vpc_ipam_preview_next_cidr" "preview_cidr" {
+  ipam_pool_id = aws_vpc_ipam_pool.mgmt.id
 
-#   vpc_public_cidr  = cidrsubnet(local.ipam_cidr, 1, 0) # Half of VPC range
-#   vpc_private_cidr = cidrsubnet(local.ipam_cidr, 1, 1) # Rest of VPC range
+  depends_on = [aws_vpc_ipam_pool_cidr.mgmt]
+}
 
-#   vpc_public_subnets  = cidrsubnets(local.vpc_public_cidr, 2, 2, 2)
-#   vpc_private_subnets = cidrsubnets(local.vpc_private_cidr, 2, 2, 2)
-# }
+locals {
+  ipam_cidr = data.aws_vpc_ipam_preview_next_cidr.preview_cidr.cidr
+
+  vpc_public_cidr  = cidrsubnet(local.ipam_cidr, 1, 0) # Half of VPC range
+  vpc_private_cidr = cidrsubnet(local.ipam_cidr, 1, 1) # Rest of VPC range
+
+  vpc_public_subnets  = cidrsubnets(local.vpc_public_cidr, 2, 2, 2)
+  vpc_private_subnets = cidrsubnets(local.vpc_private_cidr, 2, 2, 2)
+}
